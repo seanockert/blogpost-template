27c8a9044b8585da41efb59da97f71338189a41d
Sean Ockert <seanockert@gmail.com>
2018-12-13 13:59:05 +1000

    Add CSS variables

--- src/sw.js
+++ src/sw.js
@@ -6,7 +6,7 @@
 (function() {
 	// Update 'version' if you need to refresh the cache
 	var staticCacheName = 'static';
-	var version = 'v9::';
+	var version = 'v11::';
 
 	// Store core files in a cache (including a page to display when offline)
 	function updateStaticCache() {
